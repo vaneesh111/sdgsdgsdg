56e30595d608e493325674de29c91c1557684b55
vaneesh111 <vanesh016@gmail.com>
2025-03-24 09:39:42 +0800

    Update and rename credentials.json to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+ASTERISK_HOST = '10.90.235.171'
+ASTERISK_PORT = 5038
+ASTERISK_USERNAME = 'amiget'
+ASTERISK_PASSWORD = '3fda4cfa10662ebe5adc9546eadsfjhsakdfhasdf11'
+
+TELEGRAM_TOKEN = '7582971875:AAGL5pbEEkHImv5P1ZvAwMUrgyYYxfNPTT0'
+TELEGRAM_CHAT_ID = '@raketacalls'
+
+YOUGILE_API_URL = "https://ru.yougile.com/api-v2/tasks"
+YOUGILE_API_KEY = "Bearer dgSMb2NTAPnUHyAtnvrDAMAAbZuWwHoB2Dpv6E4zd0yHe0spEt9f3i0ElIqN61OM"
+YOUGILE_TEAM_ID = "07e0969d601e"
+
+WAIT_TIME_THRESHOLD = 30  # Время ожидания перед уведомлением (в секундах)
